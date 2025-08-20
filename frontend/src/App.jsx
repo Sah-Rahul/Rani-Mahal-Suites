038ad7faa7dbe28cb6505afe256cc6aae1a08981
RahulSah <sahfamilyinfo@gmail.com>
2025-08-20 21:23:12 +0545

    design landing-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <>
+    <div> <Navbar /></div>
+    <Routes>
+
+      <Route path='/' element={<Home /> } />
+    </Routes>
+    </>
   )
 }
 
